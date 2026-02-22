69f8be3f0469a8ac91e58558bd640181bb844e53
Mingle Wu <107299690+T27046@users.noreply.github.com>
2026-02-22 23:23:45 +0800

    PWA

---
+++ calculator/sw.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = 'calculator-pwa-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  '../styles2.css',
+  '../Favicon512.ico',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Lato:wght@300;400;500;600;700&display=swap'
+];
+
+// 安装Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 激活Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果缓存中有请求的资源，则返回缓存
+        if (response) {
+          return response;
+        }
+
+        // 否则从网络获取
+        return fetch(event.request).then(response => {
+          // 检查是否为有效响应
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // 克隆响应
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+      .catch(() => {
+        // 如果网络请求失败，可以返回一个离线页面
+        return caches.match('./index.html');
+      })
+  );
+});
